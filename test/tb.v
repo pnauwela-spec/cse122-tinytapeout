981d8dd5688e4d54a46f34790ae4d22028debbf1
Pieter Nauwelaerts <pnauwela@ucsc.edu>
2026-03-08 12:08:35 -0700

    testbed typos

--- test/tb.v
+++ test/tb.v
@@ -16,7 +16,7 @@ module tb_ds0 ();
   	reg clk;
   	reg rst_n;
 	reg ena;
-  	wire [7:0] ui_in;
+  	reg [7:0] ui_in;
   	wire [7:0] uo_out;
 	wire [7:0] uio_oe;
 	wire [7:0] uio;
@@ -41,8 +41,8 @@ module tb_ds0 ();
         	.ui_in(ui_in),
         	.uo_out(uo_out),
 			.uio(uio),
-			.uio(uio_oe),
-			.uio(uio_in),
+			.uio_oe(uio_oe),
+			.uio_in(uio_in),
         	.clk(clk),
         	.rst_n(rst_n),
 			.ena(ena),
@@ -55,7 +55,7 @@ module tb_ds0 ();
         	.uo_out(uo_out),
 			.uio(uio),
 			.uio_oe(uio_oe),
-			.uio(uio_in),
+			.uio_in(uio_in),
         	.clk(clk),
         	.rst_n(rst_n)
     	);
